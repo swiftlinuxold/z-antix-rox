cfc855d70d25c0e14b66d4d0808d0b32a4ee969e
Jason Hsu <github@jasonhsu.com>
2011-08-03 14:30:10 -0500

    Added exit numbers, changed chroot check

--- main.sh
+++ main.sh
@@ -5,21 +5,22 @@
 if [ ! $( id -u ) -eq 0 ]; then
 	echo "You must be root to run this script."
 	echo "Please enter su before running this script again."
-	exit
+	exit 2
 fi
 
-USERNAME=$(logname)
-IS_CHROOT=0
+IS_CHROOT=0 # changed to 1 if and only if in chroot mode
+USERNAME=""
+DIR_DEVELOP=""
 
 # The remastering process uses chroot mode.
 # Check to see if this script is operating in chroot mode.
-# If /home/$USERNAME exists, then we are not in chroot mode.
-if [ -d "/home/$USERNAME" ]; then
-	IS_CHROOT=0 # not in chroot mode
-	DIR_DEVELOP=/home/$USERNAME/develop 
-else
+# /srv directory only exists in chroot mode
+if [-d "/srv"]; then
 	IS_CHROOT=1 # in chroot mode
 	DIR_DEVELOP=/usr/local/bin/develop 
+else
+	USERNAME=$(logname) # not in chroot mode
+	DIR_DEVELOP=/home/$USERNAME/develop 
 fi
 
 echo "CHANGING ROX"
@@ -49,3 +50,5 @@ if [ $IS_CHROOT -eq 0 ]; then
 else
 	chown demo:users /usr/local/bin/pinboardtoggle.sh
 fi
+
+exit 0
